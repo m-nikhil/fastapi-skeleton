7c889205dc4a027157f29e7f3e3b47be58db844b
Nikhil Mahendran <nikhil@Nikhils-MacBook-Pro.local>
2020-04-25 14:19:52 +0530

    fix postgres env var

--- app/main.py
+++ app/main.py
@@ -5,7 +5,11 @@ from .configs import Configs
 from .database.sql import Database
 
 configs = Configs()
-db = Database(configs.connection_string, 
+db = Database(configs.postgres_user,
+                configs.postgres_password,
+                configs.postgres_host,
+                configs.postgres_port,
+                configs.postgres_db,
                 configs.max_pool_size, 
                 configs.min_pool_size)
 
