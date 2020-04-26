560df1ff89a27a80db87e7456b5d8a069c0a46ab
Nikhil Mahendran <m_nikhil@outlook.com>
2020-04-26 16:45:32 +0530

    update dto structure

--- app/api/v1/user.py
+++ app/api/v1/user.py
@@ -2,9 +2,9 @@ from typing import List
 
 from fastapi import APIRouter
 
-from app.dtos.userDto import UserPersist
-from app.dtos.userDto import UserRequest
-from app.dtos.userDto import UserResponse
+from app.dtos.userDto.persist import User as UserPersist
+from app.dtos.userDto.request import CreateUser
+from app.dtos.userDto.response import ResponseUser
 
 router = APIRouter()
 
@@ -18,24 +18,24 @@ def mock_password_hasher(raw_password: str):
     return "supersecret" + raw_password
 
 
-def mock_save_user(user_request: UserRequest):
+def mock_save_user(user_request: CreateUser):
     hashed_password = mock_password_hasher(user_request.password)
     user_persist = UserPersist(**user_request.dict(), hashed_password=hashed_password)
     print("User saved! ..not really")
     return user_persist
 
 
-@router.get("/user/", response_model=List[UserResponse])
+@router.get("/user/", response_model=List[ResponseUser])
 async def read():
     return mock_data
 
 
-@router.get("/user/me", response_model=UserResponse)
+@router.get("/user/me", response_model=ResponseUser)
 async def read_me():
     return mock_data[0]
 
 
-@router.post("/user/", response_model=UserResponse)
-async def create(user_request: UserRequest):
+@router.post("/user/", response_model=ResponseUser)
+async def create(user_request: CreateUser):
     user_saved = mock_save_user(user_request)
     return user_saved
